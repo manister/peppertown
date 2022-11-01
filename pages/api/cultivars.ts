1f0fe06cbe1efb31538e61c9d7454d1b1e08cf4e
Harry <24208175+manister@users.noreply.github.com>
2022-11-01 23:32:40 +0000

    wip

--- pages/api/cultivars.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import getChilliData from '~/lib/getChilliData'
-
-const getCultivars = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  const cultivars = await getChilliData()
-  res.status(200).send({ cultivars })
-}
-
-export default getCultivars
